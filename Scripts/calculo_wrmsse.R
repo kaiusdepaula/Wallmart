d4b81a930ec33c80a4f3c2c775522eab667495d5
kaiusdepaula <kaiusdepaulacorrea@gmail.com>
2021-09-24 10:11:57 -0300

    WRMSSE completo não otimizado

--- Scripts/calculo_wrmsse.R
+++ Scripts/calculo_wrmsse.R
@@ -6,6 +6,7 @@ library(lightgbm)
 lojas <- c("CA_1", "CA_2", "CA_3", "CA_4", "TX_1", "TX_2", "TX_3", "WI_1", 
            "WI_2", "WI_3")
 
+setwd("~/projetos/Wallmart/modelos")
 dt_train <- data.table()
 dt_valid <- data.table()
 for (loja in lojas) {
@@ -58,7 +59,7 @@ for (loja in lojas) {
 #Cálculo do WRMSSE
 
 #Nível 1
-#Retorno de 1 valor por dia
+#Retorno de 1 valor por dia - Total
 data <- dt_train[, .(sales, dia)] 
 data <- data[
   , 
@@ -86,37 +87,413 @@ weights <- 1 #Unica serie de tempo da agregação
 WRMSSE_1 <- ((1/12) * RMSSE * weights)
 
 #Nível 2
-#Retorno de 3 valores por dia
-data <- dt_train[, .(sales, dia, state_id)] 
-data <- data[
-  , 
-  .(
-    sales = sum(sales, na.rm = TRUE)
-  ), by = c("dia", "state_id")
-]
-
-data2 <- dt_valid[, .(sales, preds, dia, state_id)]
-data2 <- data2[
-  , 
-  .(
-    sales = sum(sales, na.rm = TRUE), 
-    preds = sum(preds, na.rm = TRUE)
-  ), by = c("dia", "state_id")
-]
-
-#adicionar loop
-RMSE_future <- ModelMetrics::rmse(data2$sales, data2$preds) #Numerador
-RMSE_historical <- data[, sqrt(mean((diff(sales) ** 2), na.rm = TRUE))] #Denominador
-RMSSE <- RMSE_future / RMSE_historical
+#Retorno de 3 valores por dia - State
+RMSSE <- c()
+for (state in unique(dt_valid$state_id)) {
+  data <- dt_train[, .(sales, dia, state_id)]
+  data <- data[state_id == state]
+  data <- data[
+    , 
+    .(
+      sales = sum(sales, na.rm = TRUE)
+    ), by = c("dia")
+  ]
+  
+  data2 <- dt_valid[, .(sales, preds, dia, state_id)]
+  data2 <- data2[state_id == state]
+  data2 <- data2[
+    , 
+    .(
+      sales = sum(sales, na.rm = TRUE), 
+      preds = sum(preds, na.rm = TRUE)
+    ), by = c("dia")
+  ]
+  RMSE_future <- ModelMetrics::rmse(data2$sales, data2$preds) #Numerador
+  RMSE_historical <- data[, sqrt(mean((diff(sales) ** 2), na.rm = TRUE))] #Denominador
+  RMSSE <- rbind(RMSSE, RMSE_future / RMSE_historical)
+}
 
 #weights
 data3 <- dt_valid[
   , 
   .(
     revenue = sum(revenue, na.rm = TRUE)
-  ), by = dia
+  ), by = state_id
 ]
 
-weights <- data3[, sum(revenue) / sum(revenue)]
+weights <- data3[, revenue / sum(revenue)]
 
-WRMSSE_1 <- ((1/12) * RMSSE * weights)
+WRMSSE_2 <- sum((1/12) * RMSSE * weights)
+
+#Nível 3
+#Retorno de 10 valores por dia - Store
+RMSSE <- c()
+for (store in unique(dt_valid$store_id)) {
+  data <- dt_train[, .(sales, dia, store_id)]
+  data <- data[store_id == store]
+  data <- data[
+    , 
+    .(
+      sales = sum(sales, na.rm = TRUE)
+    ), by = c("dia")
+  ]
+  
+  data2 <- dt_valid[, .(sales, preds, dia, store_id)]
+  data2 <- data2[store_id == store]
+  data2 <- data2[
+    , 
+    .(
+      sales = sum(sales, na.rm = TRUE), 
+      preds = sum(preds, na.rm = TRUE)
+    ), by = c("dia")
+  ]
+  RMSE_future <- ModelMetrics::rmse(data2$sales, data2$preds) #Numerador
+  RMSE_historical <- data[, sqrt(mean((diff(sales) ** 2), na.rm = TRUE))] #Denominador
+  RMSSE <- rbind(RMSSE, RMSE_future / RMSE_historical)
+}
+
+#weights
+data3 <- dt_valid[
+  , 
+  .(
+    revenue = sum(revenue, na.rm = TRUE)
+  ), by = store_id
+]
+
+weights <- data3[, revenue / sum(revenue)]
+
+WRMSSE_3 <- sum((1/12) * RMSSE * weights)
+
+#Nível 4
+#Retorno de 3 valores por dia - Category
+RMSSE <- c()
+for (cat in unique(dt_valid$cat_id)) {
+  data <- dt_train[, .(sales, dia, cat_id)]
+  data <- data[cat_id == cat]
+  data <- data[
+    , 
+    .(
+      sales = sum(sales, na.rm = TRUE)
+    ), by = c("dia")
+  ]
+  
+  data2 <- dt_valid[, .(sales, preds, dia, cat_id)]
+  data2 <- data2[cat_id == cat]
+  data2 <- data2[
+    , 
+    .(
+      sales = sum(sales, na.rm = TRUE), 
+      preds = sum(preds, na.rm = TRUE)
+    ), by = c("dia")
+  ]
+  RMSE_future <- ModelMetrics::rmse(data2$sales, data2$preds) #Numerador
+  RMSE_historical <- data[, sqrt(mean((diff(sales) ** 2), na.rm = TRUE))] #Denominador
+  RMSSE <- rbind(RMSSE, RMSE_future / RMSE_historical)
+}
+
+#weights
+data3 <- dt_valid[
+  , 
+  .(
+    revenue = sum(revenue, na.rm = TRUE)
+  ), by = cat_id
+]
+
+weights <- data3[, revenue / sum(revenue)]
+
+WRMSSE_4 <- sum((1/12) * RMSSE * weights)
+
+#Nível 5
+#Retorno de 7 valores por dia - Department
+RMSSE <- c()
+for (dept in unique(dt_valid$dept_id)) {
+  data <- dt_train[, .(sales, dia, dept_id)]
+  data <- data[dept_id == dept]
+  data <- data[
+    , 
+    .(
+      sales = sum(sales, na.rm = TRUE)
+    ), by = c("dia")
+  ]
+  
+  data2 <- dt_valid[, .(sales, preds, dia, dept_id)]
+  data2 <- data2[dept_id == dept]
+  data2 <- data2[
+    , 
+    .(
+      sales = sum(sales, na.rm = TRUE), 
+      preds = sum(preds, na.rm = TRUE)
+    ), by = c("dia")
+  ]
+  RMSE_future <- ModelMetrics::rmse(data2$sales, data2$preds) #Numerador
+  RMSE_historical <- data[, sqrt(mean((diff(sales) ** 2), na.rm = TRUE))] #Denominador
+  RMSSE <- rbind(RMSSE, RMSE_future / RMSE_historical)
+}
+
+#weights
+data3 <- dt_valid[
+  , 
+  .(
+    revenue = sum(revenue, na.rm = TRUE)
+  ), by = dept_id
+]
+
+weights <- data3[, revenue / sum(revenue)]
+
+WRMSSE_5 <- sum((1/12) * RMSSE * weights)
+
+#Nível 6
+#Retorno de 9 valores por dia - State, category
+RMSSE <- c()
+for (state in unique(dt_valid$state_id)) {
+  for (cat in unique(dt_valid$cat_id)) {
+    data <- dt_train[, .(sales, dia, state_id, cat_id)]
+    data <- data[state_id == state & cat_id == cat]
+    data <- data[
+      , 
+      .(
+        sales = sum(sales, na.rm = TRUE)
+      ), by = c("dia")
+    ]
+    
+    data2 <- dt_valid[, .(sales, preds, dia, state_id, cat_id)]
+    data2 <- data2[state_id == state & cat_id == cat]
+    data2 <- data2[
+      , 
+      .(
+        sales = sum(sales, na.rm = TRUE), 
+        preds = sum(preds, na.rm = TRUE)
+      ), by = c("dia")
+    ]
+    RMSE_future <- ModelMetrics::rmse(data2$sales, data2$preds) #Numerador
+    RMSE_historical <- data[, sqrt(mean((diff(sales) ** 2), na.rm = TRUE))] #Denominador
+    RMSSE <- rbind(RMSSE, RMSE_future / RMSE_historical)
+  }
+}
+
+#weights
+data3 <- dt_valid[
+  , 
+  .(
+    revenue = sum(revenue, na.rm = TRUE)
+  ), by = c("state_id", "cat_id")
+]
+
+weights <- data3[, revenue / sum(revenue)]
+
+WRMSSE_6 <- sum((1/12) * RMSSE * weights)
+
+#Nível 7
+#Retorno de 21 valores por dia - State, department
+RMSSE <- c()
+for (state in unique(dt_valid$state_id)) {
+  for (dept in unique(dt_valid$dept_id)) {
+    data <- dt_train[, .(sales, dia, state_id, dept_id)]
+    data <- data[state_id == state & dept_id == dept]
+    data <- data[
+      , 
+      .(
+        sales = sum(sales, na.rm = TRUE)
+      ), by = c("dia")
+    ]
+    
+    data2 <- dt_valid[, .(sales, preds, dia, state_id, dept_id)]
+    data2 <- data2[state_id == state & dept_id == dept]
+    data2 <- data2[
+      , 
+      .(
+        sales = sum(sales, na.rm = TRUE), 
+        preds = sum(preds, na.rm = TRUE)
+      ), by = c("dia")
+    ]
+    RMSE_future <- ModelMetrics::rmse(data2$sales, data2$preds) #Numerador
+    RMSE_historical <- data[, sqrt(mean((diff(sales) ** 2), na.rm = TRUE))] #Denominador
+    RMSSE <- rbind(RMSSE, RMSE_future / RMSE_historical)
+  }
+}
+
+#weights
+data3 <- dt_valid[
+  , 
+  .(
+    revenue = sum(revenue, na.rm = TRUE)
+  ), by = c("state_id", "dept_id")
+]
+
+weights <- data3[, revenue / sum(revenue)]
+
+WRMSSE_7 <- sum((1/12) * RMSSE * weights)
+
+#Nível 8
+#Retorno de 30 valores por dia - Store, category
+RMSSE <- c()
+for (store in unique(dt_valid$store_id)) {
+  for (cat in unique(dt_valid$cat_id)) {
+    data <- dt_train[, .(sales, dia, store_id, cat_id)]
+    data <- data[store_id == store & cat_id == cat]
+    data <- data[
+      , 
+      .(
+        sales = sum(sales, na.rm = TRUE)
+      ), by = c("dia")
+    ]
+    
+    data2 <- dt_valid[, .(sales, preds, dia, store_id, cat_id)]
+    data2 <- data2[store_id == store & cat_id == cat]
+    data2 <- data2[
+      , 
+      .(
+        sales = sum(sales, na.rm = TRUE), 
+        preds = sum(preds, na.rm = TRUE)
+      ), by = c("dia")
+    ]
+    RMSE_future <- ModelMetrics::rmse(data2$sales, data2$preds) #Numerador
+    RMSE_historical <- data[, sqrt(mean((diff(sales) ** 2), na.rm = TRUE))] #Denominador
+    RMSSE <- rbind(RMSSE, RMSE_future / RMSE_historical)
+  }
+}
+
+#weights
+data3 <- dt_valid[
+  , 
+  .(
+    revenue = sum(revenue, na.rm = TRUE)
+  ), by = c("store_id", "cat_id")
+]
+
+weights <- data3[, revenue / sum(revenue)]
+
+WRMSSE_8 <- sum((1/12) * RMSSE * weights)
+
+#Nível 9
+#Retorno de 70 valores por dia - Store, department
+RMSSE <- c()
+for (store in unique(dt_valid$store_id)) {
+  for (dept in unique(dt_valid$dept_id)) {
+    data <- dt_train[, .(sales, dia, store_id, dept_id)]
+    data <- data[store_id == store & dept_id == dept]
+    data <- data[
+      , 
+      .(
+        sales = sum(sales, na.rm = TRUE)
+      ), by = c("dia")
+    ]
+    
+    data2 <- dt_valid[, .(sales, preds, dia, store_id, dept_id)]
+    data2 <- data2[store_id == store & dept_id == dept]
+    data2 <- data2[
+      , 
+      .(
+        sales = sum(sales, na.rm = TRUE), 
+        preds = sum(preds, na.rm = TRUE)
+      ), by = c("dia")
+    ]
+    RMSE_future <- ModelMetrics::rmse(data2$sales, data2$preds) #Numerador
+    RMSE_historical <- data[, sqrt(mean((diff(sales) ** 2), na.rm = TRUE))] #Denominador
+    RMSSE <- rbind(RMSSE, RMSE_future / RMSE_historical)
+  }
+}
+
+#weights
+data3 <- dt_valid[
+  , 
+  .(
+    revenue = sum(revenue, na.rm = TRUE)
+  ), by = c("store_id", "dept_id")
+]
+
+weights <- data3[, revenue / sum(revenue)]
+
+WRMSSE_9 <- sum((1/12) * RMSSE * weights)
+
+######################### A partir daqui se torna inviável sem otimizar
+
+#Nível 10
+#Retorno de 3049 valores por dia - item_id
+RMSSE <- c()
+for (item in unique(dt_valid$item_id)) {
+  data <- dt_train[, .(sales, dia, item_id)]
+  data <- data[item_id == item]
+  
+  data2 <- dt_valid[, .(sales, preds, dia, item_id)]
+  data2 <- data2[item_id == item]
+
+  RMSE_future <- ModelMetrics::rmse(data2$sales, data2$preds) #Numerador
+  RMSE_historical <- data[, sqrt(mean((diff(sales) ** 2), na.rm = TRUE))] #Denominador
+  RMSSE <- rbind(RMSSE, RMSE_future / RMSE_historical)
+}
+
+#weights
+data3 <- dt_valid[
+  , 
+  .(
+    revenue = sum(revenue, na.rm = TRUE)
+  ), by = c("item_id")
+]
+
+weights <- data3[, revenue / sum(revenue)]
+
+WRMSSE_10 <- sum((1/12) * RMSSE * weights)
+
+#Nível 11
+#Retorno de 9147 valores por dia - item_id, state
+RMSSE <- c()
+for (state in unique(dt_valid$state_id)) {
+  for (item in unique(dt_valid$item_id)) {
+    data <- dt_train[, .(sales, dia, item_id, state_id)]
+    data <- data[item_id == item & state_id == state]
+    
+    data2 <- dt_valid[, .(sales, preds, dia, item_id, state_id)]
+    data2 <- data2[item_id == item & state_id == state]
+    
+    RMSE_future <- ModelMetrics::rmse(data2$sales, data2$preds) #Numerador
+    RMSE_historical <- data[, sqrt(mean((diff(sales) ** 2), na.rm = TRUE))] #Denominador
+    RMSSE <- rbind(RMSSE, RMSE_future / RMSE_historical)
+  }
+}
+
+
+#weights
+data3 <- dt_valid[
+  , 
+  .(
+    revenue = sum(revenue, na.rm = TRUE)
+  ), by = c("item_id", "state_id")
+]
+
+weights <- data3[, revenue / sum(revenue)]
+
+WRMSSE_11 <- sum((1/12) * RMSSE * weights)
+
+#Nível 12
+#Retorno de 30490 valores por dia - item_id, store
+RMSSE <- c()
+for (store in unique(dt_valid$store_id)) {
+  for (item in unique(dt_valid$item_id)) {
+    data <- dt_train[, .(sales, dia, item_id, store_id)]
+    data <- data[item_id == item & store_id == store]
+    
+    data2 <- dt_valid[, .(sales, preds, dia, item_id, store_id)]
+    data2 <- data2[item_id == item & store_id == store]
+    
+    RMSE_future <- ModelMetrics::rmse(data2$sales, data2$preds) #Numerador
+    RMSE_historical <- data[, sqrt(mean((diff(sales) ** 2), na.rm = TRUE))] #Denominador
+    RMSSE <- rbind(RMSSE, RMSE_future / RMSE_historical)
+  }
+}
+
+
+#weights
+data3 <- dt_valid[
+  , 
+  .(
+    revenue = sum(revenue, na.rm = TRUE)
+  ), by = c("item_id", "store_id")
+]
+
+weights <- data3[, revenue / sum(revenue)]
+
+WRMSSE_12 <- sum((1/12) * RMSSE * weights)
+
+WRMSSE <- (WRMSSE_1 + WRMSSE_2 + WRMSSE_3 + WRMSSE_4 + WRMSSE_5 + WRMSSE_6 +
+             WRMSSE_7 + WRMSSE_8 + WRMSSE_9 + WRMSSE_10 + WRMSSE_11 + WRMSSE_12)
